0b0a9e3898aefe6f27e8d46ea43460702566717c
VictorSouzaa0 <victor.r.souza05@gmail.com>
2025-03-27 11:21:54 -0300

    logics

---
+++ ExpressionLogics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ExpressionLogics
+{
+    internal class Logics
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Insira uma renda do Joaozinho");
+            double balanceJao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira uma renda  do Clebinho: ");
+            double balanceClebs =  double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+        }
+    }
+}
