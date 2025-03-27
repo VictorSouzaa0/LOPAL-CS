cd8efa2a3fecc397a8040ac13698116e9ba53127
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-03-27 04:10:27 -0300

    Primeiro de muitos

---
+++ Exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada individual
+            Console.WriteLine("Enre com seu nome completo: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Qunatos quartos tem na sua casa? ");
+            int rooms = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Quanto vale sua bicicleta? ");
+            double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            
+            //Entrada vetorizada
+            Console.WriteLine("Enter com seu último nome, idade e altura (na mesma linha): ");
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string yname = vet[0];
+            int yage = int.Parse(vet[1]);
+            double height = double.Parse(vet[2],CultureInfo.InvariantCulture);
+            
+            //Saída individual:
+            Console.WriteLine(name + "\n" + rooms + "\n" + price.ToString("F2",CultureInfo.InvariantCulture));
+            
+            //Saìda Vetorizada:
+            Console.WriteLine("Nome: " + yname + "\n" + "Idade: " + yage + "\n" + "Altura: " + height.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
+
---
+++ FirstProject/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization; 
+
+namespace FirstProject //Nome onde está sendo agrupado o programa
+{
+    internal class Program //Criando classe que só pode ser utilizada nesse escopo:
+    {
+        static void Main(String[] args)//Main é o ponto de entrada do programa
+        {
+            
+            //Saída de dados:
+            double x = 3.1456;
+            Console.WriteLine(x.ToString("F2",CultureInfo.InvariantCulture)); //CultureInfo.InvariantCulture  realiza a troca da "," de um número flutuante por um "."
+            double price = 5.00;
+            double money = 1000;
+            Console.WriteLine("O preço do refrigerante está no total de : " + 
+            price.ToString("F2", CultureInfo.InvariantCulture)
+            );
+            Console.WriteLine("Troco : " + (money - price).ToString("F2", CultureInfo.InvariantCulture));
+            
+            //Entrada de dados:
+            Console.WriteLine("Digite seu nome: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Olá " + name + " Seja bem vindo!!");
+            
+            //Usando o Split:
+            // Console.WriteLine("Insira suas marcas de carros favoritas: ");
+            // string[]Cars = Console.ReadLine().Split(' ');
+            // Console.WriteLine(Cars[0]);
+            // Console.WriteLine(Cars[1]);
+            // Console.WriteLine(Cars[2]);
+            
+            //Entrada de dados PT2:
+            Console.WriteLine("Digite suas informações como: nome, sexo e idade na mesma linha \n" +
+                              "Lembre-se de dar espaço depois de inserir cada dado!");
+            string[] vetor = Console.ReadLine().Split(' ');
+            
+            string namee = vetor[0];
+            char sex = char.Parse(vetor[1]);
+            int age = int.Parse(vetor[2]);
+            double height = double.Parse(vetor[3], CultureInfo.InvariantCulture);
+            Console.WriteLine("Nome: " +name + "\n" + "Sexo: " + sex + "\n" + "Idade: "+ age  + "\n" + height.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
+
